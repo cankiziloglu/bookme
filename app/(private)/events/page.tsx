38594a020e7a65560f4bd3f31934d5d9fc15fe7b
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-12-17 20:59:20 +0300

    refactor: update EventsPage with user authentication and event display logic; add database connection setup

--- app/(private)/events/page.tsx
+++ app/(private)/events/page.tsx
@@ -1,13 +1,44 @@
+import { Button } from '@/components/ui/button';
+import { auth } from '@clerk/nextjs/server';
+import { CalendarPlus, CalendarRange } from 'lucide-react';
+import { db } from '@/db/db';
+import Link from 'next/link';
 
+const EventsPage = async () => {
+  const { userId, redirectToSignIn } = await auth();
+  if (userId == null) return redirectToSignIn();
 
+  const events = await db.query.EventTable.findMany({
+    where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
+    orderBy: ({ createdAt }, { desc }) => desc(createdAt),
+  });
 
-const EventsPage = () => {
   return (
-
     <>
-      <div>EventsPage</div>
+      <div className='flex gap-4'>
+        <h1>Events</h1>
+        <Button asChild>
+          <Link href='/events/new'>
+            <CalendarPlus className='mr-4 size-6' /> New Event
+          </Link>
+        </Button>
+      </div>
+      {events.length > 0 ? (
+        <h1>Events</h1>
+      ) : (
+        <div className='flex flex-col items-center gap-4'>
+          <CalendarRange className='size-12 mx-auto' />
+          You do not have any events yet. Create your first event to get
+          started!
+          <Button asChild>
+            <Link href='/events/new'>
+              <CalendarPlus className='mr-4 size-6' /> New Event
+            </Link>
+          </Button>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default EventsPage
+export default EventsPage;
--- db/index.ts
+++
@@ -1,5 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-
-const sql = neon(process.env.DATABASE_URL!) as unknown as string;
-export const db = drizzle(sql);
