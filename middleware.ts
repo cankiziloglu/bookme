e9b714af9230095ac4ff6f11f8992a852e95fe31
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-12-20 21:01:17 +0300

    feat: update middleware to include booking route in public route matcher

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/book(.*)',
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-     await auth.protect()
+    await auth.protect();
   }
 });
 
